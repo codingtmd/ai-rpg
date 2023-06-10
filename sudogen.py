d1468995ae5339b980e6d37883eccfee3342fc19
codingtmd <fisher.lei@gmail.com>
2023-06-10 15:29:22 +0800

    patch1 for a MVP version

--- sudogen.py
+++ sudogen.py
@@ -1,6 +1,9 @@
 #!/bin/env python3
+import os
 import sys
 import logging
+from datetime import datetime
+import time  
 
 logging.basicConfig(level=logging.DEBUG)
 logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
@@ -12,21 +15,17 @@ rootLogger.addHandler(consoleHandler)
 
 def genHeader():
 
-    header = """// SudoLang v1.0.8
+    timestamp = time.time()
+    date_time = datetime.fromtimestamp(timestamp)
+    str_date_time = date_time.strftime("%d-%m-%Y, %H:%M:%S")
+    
+    header = """// SudoLang v0.4.0-{timestamp}
+    """.format( timestamp = str_date_time)
 
-```SudoLang
-    """
     return header
 
 def genEnding():
-    header = """```
-This is a roleplay game. I am the protagonist. Acting as the dungeon master, you are the game’s lead storyteller and referee. You run the adventure for the player, who navigate its hazards and decide which paths to explore. 
-You describe the locations and creatures in an adventure, and the players decides what they want their characters to do. Then the DM(means you), using imagination and the game's rules, determine the results of their actions and narrates what they experience.
-Because the DM can improvise to react to anything the players attempt, you need to be infinitely flexible, and make each adventure exciting and unexpected.
-
-This is a game of imagination, with no real winners or losers. The players are the heroes of the story, and you are the world. You narrate the story, and the players tell you what their characters do in it. You improvise a story based on the actions they take and the decisions they make. You also decide whether their actions are successful and describe the results, or let the dice determine the outcomes of their choices.
-
-Let's get you started!
+    header = """
 
     """
     return header
@@ -74,8 +73,9 @@ if __name__ == "__main__":
     pattenName = entryPath.rsplit(".", 1)[0]
     logging.debug('parse the file name is  %s', pattenName)
 
+    file_name = os.path.basename(entryPath) 
 
-    destPath = pattenName + ".sudo.gen"
+    destPath = "./Exports/" + file_name + ".gen"
 
     logging.debug('the new code will be genrated to %s', destPath)
 
